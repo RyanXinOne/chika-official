ed7112436c3ec39713c625d9543f5199c4a329af
RyanXin <xyz@ryanxin.cn>
2022-05-22 23:15:51 +0100

    Initialise project

---
+++ src/components/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="body">
+    Chika's official website.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {}
+}
+</script>
+
+<style lang="less" scoped>
+.body {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+
+  position: absolute;
+  width: 1512px;
+  height: 5427px;
+  left: 0px;
+  top: 0px;
+}
+</style>

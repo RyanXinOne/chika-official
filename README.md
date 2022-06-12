286f5046c000d3631000e062ec0d9f4163b1d722
RyanXin <xyz@ryanxin.cn>
2022-06-12 19:14:13 +0100

    Implement ABOUT section

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Chika Official
 
-Chika's official website.
+Music producer/mixing engineer/composer takes your song to the next lever.
+
+<p align=center><img src="./design-cover.jpg" /></p>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -16,6 +16,20 @@
         <span>EN</span>
       </div>
     </header>
+    <section id="about">
+      <div id="mask-cover">
+        <div></div>
+      </div>
+      <span id="description">Music producer/mixing engineer/composer<br>take your song to the next lever.</span>
+      <span id="big-logo">FU<span>U</span></span>
+      <div id="social-icons">
+        <img src="../assets/instagram.svg" />
+        <img src="../assets/twitter.svg" />
+        <img src="../assets/youtube.svg" />
+        <img src="../assets/wechat.svg" />
+        <img src="../assets/bilibili.svg" />
+      </div>
+    </section>
   </div>
 </template>
 
@@ -26,6 +40,9 @@ export default {
 </script>
 
 <style lang="less" scoped>
+@import 'https://api.fontshare.com/css?f[]=satoshi@300,400,500,700,900,1&display=swap';
+@import 'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap';
+
 @base-font: Helvetica, Arial, sans-serif;
 @theme-font: 'Satoshi', @base-font;
 @theme-color: #68DFE3;
@@ -80,16 +97,16 @@ export default {
       @width: 361px;
       @height: 19px;
 
+      position: absolute;
+      left: calc(50% - @width / 2);
+      top: calc(50% - @height / 2 + 17.5px);
+      width: @width;
+      height: @height;
       display: flex;
       flex-direction: row;
       align-items: flex-start;
       padding: 0px;
       gap: 36px;
-      width: @width;
-      height: @height;
-      position: absolute;
-      left: calc(50% - @width / 2);
-      top: calc(50% - @height / 2 + 17.5px);
 
       span {
         font-family: @theme-font;
@@ -124,6 +141,118 @@ export default {
         letter-spacing: 1.15px;
         text-transform: uppercase;
         color: @theme-color;
+      }
+    }
+  }
+
+  #about {
+    position: relative;
+    height: 1000px;
+    background: @theme-color;
+
+    #mask-cover {
+      @width: 856px;
+      @height: 1000px;
+
+      position: absolute;
+      top: 0px;
+      right: 0px;
+      width: @width;
+      height: @height;
+      overflow-x: hidden;
+      background: linear-gradient(180deg, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0) 40.3%), url(../assets/cover.png);
+      background-size: @width @height;
+
+      &::before {
+        content: '';
+        position: absolute;
+        top: 0px;
+        right: 0px;
+        width: 100%;
+        height: 100%;
+        background-image: radial-gradient(circle at center, @black-2 1.2px, rgba(0, 0, 0, 0) 1.2px);
+        background-size: 7px 7px;
+      }
+
+      div {
+        @angle: 30deg;
+
+        &::before {
+          content: '';
+          position: absolute;
+          top: 0px;
+          right: 204px + tan(@angle) * (@height / 2);
+          width: 100%;
+          height: 100%;
+          transform: skew(-@angle);
+          background: @theme-color;
+        }
+
+        &::after {
+          content: '';
+          position: absolute;
+          top: 0px;
+          right: -(435px + tan(@angle) * (@height / 2));
+          width: 100%;
+          height: 100%;
+          transform: skew(-@angle);
+          background: @theme-color;
+        }
+      }
+    }
+
+    #description {
+      position: absolute;
+      left: 182px;
+      top: 236px;
+
+      font-family: @theme-font;
+      font-style: normal;
+      font-weight: 500;
+      font-size: 20px;
+      line-height: 32px;
+      letter-spacing: 3px;
+      text-transform: uppercase;
+      color: @black-2;
+    }
+
+    #big-logo {
+      position: absolute;
+      left: 168px;
+      top: 274px;
+
+      font-family: @theme-font;
+      font-style: normal;
+      font-weight: 900;
+      font-size: 240px;
+      line-height: 324px;
+      letter-spacing: -30px;
+      text-transform: uppercase;
+      color: @black-2;
+
+      span {
+        position: relative;
+        left: -69px;
+      }
+    }
+
+    #social-icons {
+      @height: 37.79px;
+
+      position: absolute;
+      left: 182px;
+      bottom: 77px;
+      width: 297px;
+      height: @height;
+
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      padding: 0px;
+      gap: 22.67px;
+
+      img {
+        height: @height;
       }
     }
   }

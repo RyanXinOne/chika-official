01113c5718ec7f918e85c745a9233ff5d3f1bf5c
RyanXin <xyz@ryanxin.cn>
2022-07-31 14:59:10 +0100

    Fix the problem that scrolling albums sometimes disappear

--- src/components/HomePage/SectionDiscography.vue
+++ src/components/HomePage/SectionDiscography.vue
@@ -37,7 +37,7 @@ export default {
       isScrolling: true,
       transX: 0,
       clientWidth: document.documentElement.clientWidth || document.body.clientWidth,
-      albumSize: NaN,
+      albumSize: 216,  // same as '#discography .albums'
       albumContentOn: false,
       albumData: undefined
     }
@@ -45,7 +45,7 @@ export default {
   computed: {
     albumGroupWidth() {
       // compute width of a single album group
-      return this.albumList.length * this.albumSize || 0;
+      return this.albumList.length * this.albumSize;
     },
     albumGroups() {
       // compute number of album groups required
@@ -92,9 +92,6 @@ export default {
       });
   },
   mounted() {
-    // initialise album size
-    this.albumSize = document.querySelector('#discography .albums').offsetHeight;
-
     // start infinite scroll animation
     window.requestAnimationFrame(this.albumsInfiniteScrollAnime);
   },
